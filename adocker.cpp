534a75b896056aad8de0a57dd025d266bbaf1d7c
Truong Do <truongdq54@gmail.com>
2016-06-20 22:17:47 +0900

    Fix dkr path

--- adocker.cpp
+++ adocker.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
     }
     command += std::string(comp) + " ";
   }
-  command = "docker " + command;
+  command = "dkr " + command;
   system(command.c_str());
   return 0;
 }
