3d60b3c623e5186bed0017a44889cea243ae8840
Truong Do <truongdq54@gmail.com>
2016-06-20 21:03:35 +0900

    mount read-only if user have read access

--- adocker.cpp
+++ adocker.cpp
@@ -22,21 +22,49 @@ void split(const std::string &s, const char* delim, std::vector<std::string> & v
     }
 }
 
-bool hasAccess(const char* pathname, int mode){
-  std::string newPath(pathname);
+
+std::string getHostPath(std::string comp){
   std::vector<std::string> tmp;
   /* The docker mount command has 2 forms: "-v /host_path" or "-v /host_path:/container_path"
    * so we will split the command based on ":" and take the first item
    */
-  split(newPath, ":", tmp);
+  split(comp, ":", tmp);
   std::string hostPath = tmp[0];
-  return access(hostPath.c_str(), mode);
+  return hostPath;
+}
+
+bool hasAccess(const char* pathname, int mode){
+  std::string newPath(pathname);
+  std::string hostPath = getHostPath(newPath);
+  if (access(hostPath.c_str(), mode) == 0)
+    return true;
+  return false;
+}
+
+bool mountAsRO(std::string comp){
+  int path_length = comp.size();
+  if (path_length < 3)
+    return false;
+
+  std::string mode = comp.substr(path_length - 3, path_length);
+  if (mode == ":ro"){
+    return true;
+  }
+  return false;
 }
 
 bool check_validity(const char* comp){
   std::string tmp(comp);
-  if (hasAccess(comp, W_OK) != 0){
-    std::cerr << "You don't have write permission on \"" << tmp << "\"" <<  std::endl;
+  if (!hasAccess(comp, W_OK)){
+    if (hasAccess(comp, R_OK)){
+      if (mountAsRO(tmp)){
+        return true;
+      }else{
+        std::cerr << "You only have read permission on \"" << tmp << "\"" << " please mount it as read-only volume" <<  std::endl;
+        return false;
+      }
+    }
+    std::cerr << "You don't have any permission on \"" << tmp << "\"" <<  std::endl;
     return false;
   }
   return true;
